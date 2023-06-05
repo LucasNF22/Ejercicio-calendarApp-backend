a1cbe649101b8ac9055c1cef4b41bb702e3d6a08
LucasNF22 <lucasnfiorentino@gmail.com>
2023-06-05 10:02:22 -0300

    Configuracion inicial de Express en el puerto 4000

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hola mundo');
+const express = require("express");
+
+
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get('/', ( req, res ) => {
+    console.log('se requiere el /');
+    res.json({
+        ok: true
+    })
+});
+
+
+
+// Escuchar peticiones 
+const puertoServer = 4000;
+app.listen( puertoServer, () => {
+    console.log(`Servidor corriendo en el puerto ${puertoServer}`);
+
+});
