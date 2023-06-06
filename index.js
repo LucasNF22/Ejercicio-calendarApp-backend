56bce771915784ad247dca11629c259dc7d4a5b9
LucasNF22 <lucasnfiorentino@gmail.com>
2023-06-06 15:06:16 -0300

    Configuracion de variables de entorno y estructura basica de carpetas.

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const express = require("express");
+require('dotenv').config();
 
+// console.log(process.env);
 
 // Crear el servidor de express
 const app = express();
 
+// Dictorio Publico
+app.use( express.static('public') );
+
+
 // Rutas
-app.get('/', ( req, res ) => {
-    console.log('se requiere el /');
-    res.json({
-        ok: true
-    })
-});
+// app.get('/', ( req, res ) => {
+//     console.log('se requiere el /');
+//     res.json({
+//         ok: true
+//     })
+// });
 
 
 
 // Escuchar peticiones 
 const puertoServer = 4000;
-app.listen( puertoServer, () => {
-    console.log(`Servidor corriendo en el puerto ${puertoServer}`);
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 
 });
