26e618e5454c95b53dd222dd1fb09f0de708fdd1
LucasNF22 <lucasnfiorentino@gmail.com>
2023-07-24 17:26:08 -0300

    Se creo controlador para eliminar eventos

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const Evento = require('../models/evento');
 const { body } = require('express-validator');
 const Usuario = require('../models/Usuario');
+const mongoose = require('mongoose');
 
 
 const getEventos = async( req, res = response ) => {
@@ -95,12 +96,51 @@ const UpdateEvento = async( req, res = response ) => {
    
 };
 
-const deleteEvento = ( req, res = response ) => {
-    res.json(
-        {
-            ok: true,
-            msg: 'deleteEvento'
+const deleteEvento = async( req, res = response ) => {
+    
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const evento = await Evento.findById( eventoId );
+
+        if( !evento ){
+            return res.status(500).json({
+                ok: false,
+                msg: "No existe evento con ese ID"
+            })
+        };
+        
+        // console.log( uid );
+        // console.log( vento.user.toString() );
+
+        if( evento.user.toString() !== uid ){
+            return res.status(401).json({
+                user: evento.user.toString(),
+                ok: false,
+                msg: "no tiene privilegios para Eliminar este evento"
+            })
+        };
+
+    
+        await Evento.findByIdAndRemove( eventoId );
+
+        res.json({
+            ok: true, 
+            evento: "Evento Eliminado",
         })
+
+
+
+    } catch (error) {
+        
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: "Hable con el administrador."
+        })
+    };
+
 };
 
 
