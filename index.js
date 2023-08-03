3a83c0edcaac14935951299720f75b6864984c1f
LucasNF22 <lucasnfiorentino@gmail.com>
2023-08-03 15:23:32 -0300

    prueba url 4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require('dotenv').config();
 const cors = require("cors");
 const { dbConection } = require('./database/config')
-const path = require('path');
+// const path = require('path');
 
 // console.log(process.env);
 
@@ -16,8 +16,8 @@ dbConection();
 app.use(cors());
 
 // Dictorio Publico
-// app.use( express.static('public') );
-app.use(express.static(path.join(__dirname, 'public')))
+app.use( express.static('public') );
+// app.use(express.static(path.join(__dirname, 'public')))
 
 // Lectura y parseo del body
 app.use( express.json() );
@@ -28,12 +28,12 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
 
-// app.get('/*', (req, res)=> {
-//     res.sendFile( __dirname + '/public/index.html' );
-// })
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.get('/*', (req, res)=> {
+    res.sendFile( __dirname + '/public/index.html' );
+})
+// app.get('/*', function(req,res) {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 
 
